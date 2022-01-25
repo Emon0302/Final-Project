d689cb286c637098111f05069479fd447bbdd24e
Emon0302 <emonpaul0302@gmail.com>
2022-01-25 19:49:22 +0600

    25 jan

--- admin/update_users.php
+++ admin/update_users.php
@@ -73,7 +73,6 @@ if(isset($_POST['submit'] ))
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
     <title>Dashboard-Online Event Management</title>
     <!-- Bootstrap Core CSS -->
     <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
