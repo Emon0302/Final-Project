d7921499c4171bf36d8da8ecc8ee9ce80725b1cc
Emon0302 <emonpaul0302@gmail.com>
2022-02-11 16:52:07 +0600

    11 Feb

--- admin/add_service.php
+++ admin/add_service.php
@@ -26,7 +26,7 @@ if(isset($_POST['submit']))           //if upload btn is pressed
 		  
 		
 		
-		if(empty($_POST['c_name'])||empty($_POST['res_name'])||$_POST['email']==''||$_POST['phone']==''||$_POST['url']==''||$_POST['o_hr']==''||$_POST['c_hr']==''||$_POST['o_days']==''||$_POST['address']=='')
+		if(empty($_POST['c_name'])||empty($_POST['res_name'])||$_POST['address']=='')
 		{	
 											$error = 	'<div class="alert alert-danger alert-dismissible fade show">
 																<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
